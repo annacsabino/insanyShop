3a9822440ce9c58abea521558272638ea27099f7
annacsabino <annacsabino.dev@gmail.com>
2025-07-30 00:11:24 -0300

    chore: creation of the initial component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Container } from '@/components/shared/Container.styles'
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <Container></Container>
     </main>
-  );
+  )
 }
---
+++ src/components/shared/Container.styles.ts
@@ -0,0 +1,10 @@
+'use client'
+
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: calc(1120px + 2rem);
+  margin: 0 auto;
+  padding: 0 1rem;
+`
