7fc1066ebbdb1a1edaf06eb497bd1ce508dfa777
annacsabino <annacsabino.dev@gmail.com>
2025-08-03 00:07:32 -0300

    style: padding bottom and responsive configuration

--- src/components/ProductSection/style.ts
+++ src/components/ProductSection/style.ts
@@ -2,9 +2,22 @@
 
 import styled from 'styled-components'
 
+export const ProductSectionWrapper = styled.section`
+  padding-bottom: 6.19rem;
+`
+
 export const ProductsWrapper = styled.div`
   display: grid;
   margin-top: 2.94rem;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 1.63rem;
+
+  @media (max-width: 856px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: 550px) {
+    grid-template-columns: 1fr;
+    justify-items: center;
+  }
 `
