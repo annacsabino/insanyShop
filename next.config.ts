7702aa3dae139ceef2c886b43f6204983a9e1cf3
annacsabino <annacsabino.dev@gmail.com>
2025-08-04 23:31:58 -0300

    refactor: configure styled compoenents

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  compiler: {
+    styledComponents: true
+  }
+}
 
 module.exports = {
   images: {
